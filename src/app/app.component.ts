8b73490efb574f7c78eb0883b32a68366fc4a13f
Roberto <jrlopez233@gmail.com>
2017-03-07 13:05:02 -0600

    Dummy Data
    Falta corregir problema con estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,69 @@ import { SeccionComponent } from './seccion/seccion.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  secciones = [
+	  {
+	  	"seccion" : "Estrenos",
+	  	"peliculas" : [
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	},
+	  	{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	}
+	  	]
+	  },
+	  {
+	  	"seccion" : "Infantiles",
+	  	"peliculas" : [{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	}]
+	  },
+	  {
+	  	"seccion" : "Terror",
+	  	"peliculas" : [{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	}]
+	  },
+	  {
+	  	"seccion" : "Accion",
+	  	"peliculas" : [{
+	  		"titulo": "Titulo 1",
+	  		"poster" : "/assets/imgs/posters/movie1.jpg"
+	  	}]
+	  },
+
+
+  ];
 }
--- src/app/seccion/seccion.component.ts
+++ src/app/seccion/seccion.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PeliculaComponent } from "../pelicula/pelicula.component";
 
 
 @Component({
-  selector: 'app-seccion',
+  selector: 'seccion',
   templateUrl: './seccion.component.html',
 })
 export class SeccionComponent implements OnInit {
-
+	@Input()
+	sec : any
   constructor() { }
 
   ngOnInit() {
